55335f6ae80a4367626da77ca9c564225ef95d99
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-12-19 12:41:00 +0600

    Comment

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,24 +10,28 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     public function index(){
+        // Today Sales
         $todaySalesTotal = DB::table('sales')
                         ->whereDate('created_at', Carbon::today())
                         ->sum('total_price');
-
+        // Yesterday Sales
         $yesterdaySalesTotal = DB::table('sales')
                         ->whereDate('created_at', Carbon::yesterday())
                         ->sum('total_price');
 
+        // This Month Sales
         $thisMonthSalesTotal = DB::table('sales')
                         ->whereMonth('created_at', Carbon::now()->month)
                         ->sum('total_price');
 
+        // Last Month Sales
         $lastMonth = Carbon::now()->subMonth();
         $lastMonthNumber = $lastMonth->month;
 
         $lastMonthSalesTotal = DB::table('sales')
                         ->whereMonth('created_at', $lastMonthNumber)
                         ->sum('total_price');
+
         return view ("backend.dashboard.index", compact('todaySalesTotal', 'yesterdaySalesTotal', 'thisMonthSalesTotal', 'lastMonthSalesTotal'));
     }
 }
