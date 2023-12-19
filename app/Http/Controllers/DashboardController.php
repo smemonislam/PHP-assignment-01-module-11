3bc865162d76b98a96b7faf467e9f2efbaddf9cd
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-12-19 12:26:20 +0600

    Today, Yesteday, This Month, Last Month Sales Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view ("backend.dashboard.index");
+        $todaySalesTotal = DB::table('sales')
+                        ->whereDate('created_at', Carbon::today())
+                        ->sum('total_price');
+
+        $yesterdaySalesTotal = DB::table('sales')
+                        ->whereDate('created_at', Carbon::yesterday())
+                        ->sum('total_price');
+
+        $thisMonthSalesTotal = DB::table('sales')
+                        ->whereMonth('created_at', Carbon::now()->month)
+                        ->sum('total_price');
+
+        $lastMonth = Carbon::now()->subMonth();
+        $lastMonthNumber = $lastMonth->month;
+
+        $lastMonthSalesTotal = DB::table('sales')
+                        ->whereMonth('created_at', $lastMonthNumber)
+                        ->sum('total_price');
+        return view ("backend.dashboard.index", compact('todaySalesTotal', 'yesterdaySalesTotal', 'thisMonthSalesTotal', 'lastMonthSalesTotal'));
     }
 }
